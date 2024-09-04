4df031a9fa0cdb934b02dc81cebaba5fa5dc7ca7
Mathias Weiden <mtweiden@berkeley.edu>
2024-09-04 09:58:20 -0400

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 """Set up file for bqskit-ft."""
-from setuptools import setup, find_packages
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='bqskit-ft',
@@ -7,5 +10,5 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=['bqskit', 'numpy', 'scipy'],
-    python_requires='>=3.8, <4.0'
+    python_requires='>=3.8, <4.0',
 )

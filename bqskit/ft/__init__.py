f91e4664ee641e98286134a334e92b20bc355748
Mathias Weiden <mtweiden@berkeley.edu>
2024-09-04 09:55:51 -0400

    Added ft/__init__.py

--- bqskit/ft/__init__.py
+++ bqskit/ft/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from bqskit.ft.cliffordt.cliffordtmodel import CliffordTModel
 from bqskit.ft.ftmodel import FaultTolerantModel
 from bqskit.ft.rules.replacement import ReplacementRule

900ac6661084a37a6d3e9a7cf40d02aeae178c60
Mathias Weiden <mtweiden@berkeley.edu>
2024-09-04 09:53:25 -0400

    Removed unused imports

--- bqskit/ft/cliffordt/cliffordtgates.py
+++ bqskit/ft/cliffordt/cliffordtgates.py
@@ -1,23 +1,18 @@
 from __future__ import annotations
 
-import numpy as np
-from bqskit.ir.circuit import Circuit
-from bqskit.ir.gates import CircuitGate
-
 from bqskit.ir.gates.constant.cx import CNOTGate
 from bqskit.ir.gates.constant.cz import CZGate
 from bqskit.ir.gates.constant.h import HGate
-from bqskit.ir.gates.constant.identity import IdentityGate
-from bqskit.ir.gates.parameterized.rz import RZGate
-from bqskit.ir.gates.constant.sdg import SdgGate
 from bqskit.ir.gates.constant.s import SGate
-from bqskit.ir.gates.constant.sx import SqrtXGate
+from bqskit.ir.gates.constant.sdg import SdgGate
 from bqskit.ir.gates.constant.swap import SwapGate
-from bqskit.ir.gates.constant.tdg import TdgGate
+from bqskit.ir.gates.constant.sx import SqrtXGate
 from bqskit.ir.gates.constant.t import TGate
+from bqskit.ir.gates.constant.tdg import TdgGate
 from bqskit.ir.gates.constant.x import XGate
 from bqskit.ir.gates.constant.y import YGate
 from bqskit.ir.gates.constant.z import ZGate
+from bqskit.ir.gates.parameterized.rz import RZGate
 
 
 clifford_gates = [

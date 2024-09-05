1e0d4b37d9738725d54b34bb26464a79509f1fb5
Mathias Weiden <mtweiden@berkeley.edu>
2024-09-05 13:09:56 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 """Set up file for bqskit-ft."""
 from __future__ import annotations
 
-from setuptools import find_packages
+from setuptools import find_namespace_packages
 from setuptools import setup
 
 setup(
     name='bqskit-ft',
     description='BQSKit extension for compiling to Fault-Tolerant gate sets.',
+    author='Mathias Weiden',
+    author_email='mtweiden@berkeley.edu',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_namespace_packages(exclude=['tests']),
     install_requires=['bqskit', 'numpy', 'scipy'],
     python_requires='>=3.8, <4.0',
 )

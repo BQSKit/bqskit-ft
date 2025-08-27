636ca85a375ba3be5e427b74f744ee70e532680f
Mathias Weiden <mtweiden@berkeley.edu>
2025-08-27 08:37:52 -0700

    Updated pass list

--- bqskit/ft/__init__.py
+++ bqskit/ft/__init__.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 from bqskit.ft.cliffordt.cliffordtmodel import CliffordTModel
 from bqskit.ft.ftmodel import FaultTolerantModel
 from bqskit.ft.rules.replacement import ReplacementRule
+from bqskit.ft.rules.isolate_rz import IsolateRZGatePass
 from bqskit.ft.ftpasses.gridsynth import GridSynthPass
+from bqskit.ft.ftpasses.rounding import RoundToDiscreteZPass
     
 
 __all__ = [
     'CliffordTModel',
     'FaultTolerantModel',
     'ReplacementRule',
+    'IsolateRZGatePass',
     'GridSynthPass',
+    'RoundToDiscreteZPass',
 ]

8d8976d519621359bb977bd0d1567b976ea996a7
Mathias Weiden <mtweiden@berkeley.edu>
2025-08-27 11:41:14 -0700

    Updated project structure

--- setup.py
+++ setup.py
@@ -4,13 +4,7 @@ from __future__ import annotations
 from setuptools import find_namespace_packages
 from setuptools import setup
 
+# Package configuration now in pyproject.toml
 setup(
-    name='bqskit-ft',
-    description='BQSKit extension for compiling to Fault-Tolerant gate sets.',
-    author='Mathias Weiden',
-    author_email='mtweiden@berkeley.edu',
-    version='0.1.0',
     packages=find_namespace_packages(exclude=['tests']),
-    install_requires=['bqskit', 'numpy', 'scipy', 'pygridsynth'],
-    python_requires='>=3.8, <4.0',
 )

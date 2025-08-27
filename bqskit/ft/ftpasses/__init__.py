3146ff6629806c36ea43d2445cc391bde9349e7d
Mathias Weiden <mtweiden@berkeley.edu>
2025-08-27 08:51:46 -0700

    Updated pass list

--- bqskit/ft/ftpasses/__init__.py
+++ bqskit/ft/ftpasses/__init__.py
@@ -2,7 +2,9 @@
 from __future__ import annotations
 
 from bqskit.ft.ftpasses.gridsynth import GridSynthPass
+from bqskit.ft.ftpasses.rounding import RoundToDiscreteZPass
 
 __all__ = [
     'GridSynthPass',
+    'RoundToDiscreteZPass',
 ]

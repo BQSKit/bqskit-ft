0de2bd661b95af4f3328d13a7ef6b6f8686d5560
Mathias Weiden <mtweiden@berkeley.edu>
2026-02-07 21:54:11 -0800

    Added better importing

--- bqskit/ft/cliffordrz/__init__.py
+++ bqskit/ft/cliffordrz/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from bqskit.ft.cliffordrz.cliffordrzmodel import CliffordRZModel
 from bqskit.ft.cliffordrz.defaultworkflow import build_cliffordrz_workflow
 
--- bqskit/ft/cliffordt/__init__.py
+++ bqskit/ft/cliffordt/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from bqskit.ft.cliffordt.cliffordtmodel import CliffordTModel
 from bqskit.ft.cliffordt.defaultworkflow import build_cliffordt_workflow
 

8daf28588297f91f6d4b2528b6751804995a8959
Justin Kalloor <justin.kalloor@gmail.com>
2026-02-19 13:31:27 -0800

    Adding to init

--- bqskit/ft/ftpasses/__init__.py
+++ bqskit/ft/ftpasses/__init__.py
@@ -1,10 +1,14 @@
 """Fault-tolerant synthesis passes for BQSKit."""
 from __future__ import annotations
 
+from bqskit.ft.ftpasses.convert_to_pkac import ConvertToPKAC
 from bqskit.ft.ftpasses.gridsynth import GridSynthPass
+from bqskit.ft.ftpasses.pkac_to_gates import PKACtoGatesPass
 from bqskit.ft.ftpasses.rounding import RoundToDiscreteZPass
 
 __all__ = [
     'GridSynthPass',
     'RoundToDiscreteZPass',
+    'ConvertToPKAC',
+    'PKACtoGatesPass',
 ]

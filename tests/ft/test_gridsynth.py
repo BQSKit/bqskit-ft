125d70ef5a4cb2d6757383c4a7b9da3a64c5f1b5
Justin Kalloor <justin.kalloor@gmail.com>
2026-07-06 16:45:18 -0700

    Fixing Tests

--- tests/ft/test_gridsynth.py
+++ tests/ft/test_gridsynth.py
@@ -7,7 +7,6 @@ from random import random
 from bqskit.compiler import Compiler
 from bqskit.ft.cliffordt.cliffordtgates import clifford_t_gates
 from bqskit.ft.ftpasses.gridsynth import GridSynthPass
-from bqskit.ft.rules.isolate_rz import IsolateRZGatePass
 from bqskit.ir import Circuit
 from bqskit.ir.gates import CNOTGate
 from bqskit.ir.gates import RZGate
@@ -27,7 +26,8 @@ class TestGridSynthPass:
             theta = random() * 2 * pi
             circuit.append_gate(RZGate(), [0], [theta])
 
-            gridsynth = GridSynthPass(precision=20)
+            total_error = 1e-10
+            gridsynth = GridSynthPass(algorithmic_error=total_error)
             old_circuit = circuit.copy()
 
             with Compiler() as compiler:
@@ -39,7 +39,7 @@ class TestGridSynthPass:
             old_utry = old_circuit.get_unitary()
             new_utry = new_circuit.get_unitary()
 
-            assert old_utry.get_distance_from(new_utry) < 1e-8
+            assert old_utry.get_distance_from(new_utry) < total_error
 
     def test_gridsynth_in_circuit(self) -> None:
         circuit = Circuit(2)
@@ -50,20 +50,20 @@ class TestGridSynthPass:
         circuit.append_gate(RZGate(), [1], [theta1])
 
         old_circuit = circuit.copy()
+        total_error = 1e-10
 
         passes = [
-            IsolateRZGatePass(),
-            ForEachBlockPass([GridSynthPass(precision=20)]),
-            UnfoldPass(),
+            GridSynthPass(algorithmic_error=total_error)
         ]
 
         with Compiler() as compiler:
             new_circuit = compiler.compile(circuit, passes)
 
         for op in new_circuit:
-            assert op.gate in clifford_t_gates or op.gate == U3Gate()
+            assert op.gate in clifford_t_gates
 
         old_utry = old_circuit.get_unitary()
         new_utry = new_circuit.get_unitary()
 
-        assert old_utry.get_distance_from(new_utry) < 1e-8
+        assert new_circuit.num_params == 0
+        assert old_utry.get_distance_from(new_utry) < total_error
